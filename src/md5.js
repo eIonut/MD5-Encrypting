6ac7ebdaf40f1693d3b0c88e354d95ccd140d6c0
Ionut <enacheionut2399@gmail.com>
2024-01-13 14:34:21 +0200

    added comments

--- src/md5.js
+++ src/md5.js
@@ -1,48 +1,45 @@
 const md5 = (message) => {
-  console.log("aici");
+  // Funcția de rotație la stânga, utilizată în calculele MD5
   function leftRotate(value, amount) {
     return (value << amount) | (value >>> (32 - amount));
-    //left bit rotation on a 32-bit number
   }
 
-  //shift amount sused in the main loop
+  // Valorile de shift utilizate în algoritm
   var s = [7, 12, 17, 22, 5, 9, 14, 20, 4, 11, 16, 23, 6, 10, 15, 21];
+
+  // Inițializarea tabelului de valori folosit în algoritmul MD5
   var K = [];
   for (var i = 0; i < 64; i++) {
     K[i] = Math.floor(Math.abs(Math.sin(i + 1)) * Math.pow(2, 32));
   }
 
-  // an array k of 64 elements fiecare computata la un sine de integers
-
+  // Inițializarea valorilor hash. Acestea sunt valorile de start pentru procesarea MD5
   var H0 = 0x67452301;
   var H1 = 0xefcdab89;
   var H2 = 0x98badcfe;
   var H3 = 0x10325476;
 
-  //valoriile de hash initiale -> 32 bit words h0 h1 h2 h3 care vor produce hash-ul final
-
+  // Transformarea mesajului într-un array de bytes și adăugarea padding-ului
   var bytes = [];
   for (let i = 0; i < message.length; i++) {
     bytes[i] = message.charCodeAt(i);
   }
-  bytes.push(0x80);
+  bytes.push(0x80); // Adaugă un bit de 1 la sfârșitul mesajului
 
-  // converteste mesajul intr-un array de ASCII byte values si appenduieste 0x80 adica 128 in decimal pentru aincepe
-
+  // Asigură-te că lungimea mesajului în bytes este 56 % 64
   while (bytes.length % 64 !== 56) {
     bytes.push(0x00);
   }
 
-  // continua padding-ul pana cand lungimea este 56 bytes modulo 64
-
+  // Adăuga lungimea originală a mesajului în biți la sfârșitul mesajului
   var bitLen = message.length * 8;
   var bitLenBytes = [];
   for (let i = 0; i < 8; i++) {
     bitLenBytes[i] = (bitLen >>> (i * 8)) & 255;
   }
   bytes = bytes.concat(bitLenBytes);
-  // apenduieste lungimea msj originakl in bits ca un 64 bit little indian
 
+  // Procesarea fiecărui bloc de 64 bytes
   for (let i = 0; i < bytes.length; i += 64) {
     var M = bytes.slice(i, i + 64);
     var W = [];
@@ -54,11 +51,13 @@ const md5 = (message) => {
         (M[j * 4 + 3] << 24);
     }
 
+    // Inițializarea variabilelor pentru acest bloc
     var a = H0;
     var b = H1;
     var c = H2;
     var d = H3;
 
+    // Cele 64 de iterații ale algoritmului MD5
     for (let j = 0; j < 64; j++) {
       var f, g;
       if (j < 16) {
@@ -84,11 +83,14 @@ const md5 = (message) => {
       a = temp;
     }
 
+    // Actualizează valorile hash după procesarea blocului
     H0 = (H0 + a) | 0;
     H1 = (H1 + b) | 0;
     H2 = (H2 + c) | 0;
     H3 = (H3 + d) | 0;
   }
+
+  // Convertește valorile hash într-un string hex
 
   function toHexString(num) {
     var hex = "";
@@ -101,7 +103,5 @@ const md5 = (message) => {
 
   return toHexString(H0) + toHexString(H1) + toHexString(H2) + toHexString(H3);
 };
-
-console.log(md5("Your message here..."));
 
 export default md5;
